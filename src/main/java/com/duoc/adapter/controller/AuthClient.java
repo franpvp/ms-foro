96d3467b308cf2f0abce2343a6a89a93e3238b0b
franpvp <franpvp.98@gmail.com>
2025-04-14 22:45:38 -0400

    Se agrega Dockerfile y docker-compose

--- src/main/java/com/duoc/adapter/controller/AuthClient.java
+++ src/main/java/com/duoc/adapter/controller/AuthClient.java
@@ -5,7 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "auth-service", url = "http://localhost:8081/api/v1/auth")
+
+
+@FeignClient(name = "auth-service", url = "http://spring-gestion-usuarios:8081/api/v1/auth")
 public interface AuthClient {
 
     // Verifica si el usuario está autenticado
--- src/main/java/com/duoc/adapter/controller/UsuarioClient.java
+++ src/main/java/com/duoc/adapter/controller/UsuarioClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "usuario-service", url = "http://localhost:8081/api/v1/usuarios")
+@FeignClient(name = "usuario-service", url = "http://spring-gestion-usuarios:8081/api/v1/usuarios")
 public interface UsuarioClient {
 
     // Validamos si el usuario existe en el microservicio que gestiona a los usuarios
